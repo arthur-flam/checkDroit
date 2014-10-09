6409a3b6ab86f99e1857f3b719c880c313cf4ece
Arthur Flam <arthur.flam@gmail.com>
2014-10-09 14:23:10 +0000

    basic auth

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+var prompt = require('prompt');
 var Nightmare = require('nightmare');
 
-new Nightmare()
-    .goto('https://cas.univ-paris1.fr/cas/login?service=https://esup.univ-paris1.fr/Login')
-    .type('input[id="username"]', 'id')
-    .type('input[id="password"]', 'pass')
-    .click('.btn-submit')
-    .wait()
-    .run(function (err, nightmare) {
-	if (err) return console.log(err);
-	console.log('Done!');
-    });
+prompt.start();
+prompt.message="";
+prompt.delimiter = "";
+
+prompt.get(['username', 'password'], function (err, result) {
+    if (err) return console.log(err);
+    new Nightmare()
+	.useragent("Pour avoir les admissibilites en avance - @ArthurFlam")
+	.goto('https://cas.univ-paris1.fr/cas/login?service=https://esup.univ-paris1.fr/Login')
+	.type('input[id="username"]', result.username)
+	.type('input[id="password"]', result.password)
+	.click('.btn-submit')
+	.wait()
+        .evaluate(function (page){
+	    return document.documentElement.innerHTML;
+	}, function (res){console.log(res);})
+	.run (function (err, nightmare) {
+	    if (err) return console.log(err);
+	    console.log('Done!');
+	});
+});
+
